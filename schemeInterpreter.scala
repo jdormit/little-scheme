f71d9cc622f98d6b8e0e1e2a30138b39f9048821
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2017-03-08 08:33:03 -0700

    Support reading from stdin or a file

--- schemeInterpreter.scala
+++ schemeInterpreter.scala
@@ -1,15 +1,37 @@
 import io._
 import expr._
 import java.io._
+import java.nio.file.{Paths, Files}
+import java.util.Scanner
 
 object schemeInterpreter {
   def main(args: Array[String]): Unit = {
-    if (args.length == 0) {
-      System.err.println("USAGE: scheme FILE")
-      System.exit(1)
-    }
     try {
-      val text = Source.fromFile(args(0)).mkString
+      var text = ""
+      if (args.length == 0) {
+        if (System.in.available() > 0) {
+          // Check for input from System.in
+          val sc = new Scanner(System.in)
+          if (sc.hasNextLine()) {
+            var next = ""
+            while (sc.hasNextLine()) {
+              next = sc.nextLine()
+              text += next
+            }
+          }
+        }
+        else {
+          // Otherwise, print an error
+          System.err.println("USAGE: scheme <FILE or STRING>")
+          System.exit(1)
+        }
+      }
+      else if (Files.exists(Paths.get(args(0)))) {
+        text = Source.fromFile(args(0)).mkString
+      }
+      else {
+        text = args.mkString(" ")
+      }
       val result = evalProgram(text)
       println(result)
     }
